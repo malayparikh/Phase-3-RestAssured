26c965ad810b40aaab8750ba93c41e17025a48c5
Malay Parikh <63942863+malayparikh@users.noreply.github.com>
2023-12-18 13:12:34 +0530

    Create BDD_demo_RestAssured.java

---
+++ BDD_demo_RestAssured.java
@@ -0,0 +1,53 @@
+Feature: Test Github APis using Rest Assured
+
+  Scenario: Get all the github repos with autorization as Bearer token
+    Given User Enters github base URL and Authorization
+    When User executes HTTP get method
+    Then Validate the response status code
+=========================================================
+
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+
+import org.testng.Assert;
+
+public class GithubSteps {
+	
+	Response res;
+	
+	
+	@Given("User Enters github base URL and Authorization")
+	public void user_enters_github_base_url_and_authorization() {
+		
+		given().baseUri("https://api.github.com").basePath("/user/repos")
+		.header("Authorization","Bearer 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");
+		
+		
+	}
+	  
+	
+
+	@When("User executes HTTP get method")
+	public void user_executes_http_get_method() {
+		
+		res = given().baseUri("https://api.github.com").basePath("/user/repos")
+				.header("Authorization","Bearer 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")
+				.when().get();
+	 
+	}
+
+	@Then("Validate the response status code")
+	public void validate_the_response_status_code() {
+		
+		res.prettyPrint();
+		Assert.assertEquals(res.statusCode(), 200);
+		
+	}
+	 
+
+}
