055dd2f20849bf7f6816398b53502865f393fced
Malay Parikh <63942863+malayparikh@users.noreply.github.com>
2023-12-19 10:03:27 +0530

    Update Script010_LoggingMethods.java

--- Script010_LoggingMethods.java
+++ Script010_LoggingMethods.java
@@ -10,7 +10,7 @@ public class Script010_LoggingMethods {
 	@Test
 	public void LoggingRestAssured() {
 		
-		String PMapikey = "PMAK-6581111972602c4ddddad9f0-5b8c9b84cb6c486e5773965eea4126fd46";
+		String PMapikey = "PMAK-use your key here";
 		
 		given().baseUri("https://api.postman.com")
 		.basePath("/workspaces")
